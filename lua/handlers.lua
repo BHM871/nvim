53326f4a49a093610c3b62e9041fca3d7012fe25
Adrian Almeida <adrianalmeida417@gmail.com>
2025-06-19 19:59:04 -0300

    fix: renaming terminal buffer

--- lua/handlers.lua
+++ lua/handlers.lua
@@ -20,6 +20,7 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.open_terminal = function ()
   vim.cmd('split')
   vim.cmd('res -10')
+  vim.cmd('file terminal')
 
   local bufs = vim.api.nvim_list_bufs()
 
