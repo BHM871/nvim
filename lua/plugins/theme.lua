7ba425743ac062338529a3280fe5f11d3ec9c1e5
Adrian Almeida <adrianalmeida417@gmail.com>
2025-06-28 15:41:52 -0300

    chore: improve theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,36 @@
 return {
-  "olimorris/onedarkpro.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme onedark")
-  end
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      local onedark = require("onedark")
+      onedark.setup({
+        style = 'darker'
+      })
+      onedark.load()
+    end
+  },
+  {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      vim.opt.termguicolors = true
+      require("bufferline").setup({
+        options = {
+          separator_style = "slant",
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(_, _, diagnostics_dict, _)
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+              local sym = e == "error" and " "
+                  or (e == "warning" and " " or " ")
+              s = s .. n .. sym
+            end
+            return s
+          end
+        }
+      })
+    end
+  }
 }
