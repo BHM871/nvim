2c988530a3d9a3777742dc43d259dfb6fbf83011
Adrian Almeida <adrianalmeida417@gmail.com>
2026-03-30 16:02:06 -0300

    chore: Adding new keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -44,3 +44,5 @@ keymap("t", "<C-h>", "<C-\\><C-n><C-w>h", opts)
 keymap("t", "<C-j>", "<C-\\><C-n><C-w>j", opts)
 keymap("t", "<C-k>", "<C-\\><C-n><C-w>k", opts)
 keymap("t", "<C-l>", "<C-\\><C-n><C-w>l", opts)
+
+keymap("t", "<C-t>", "<C-\\><C-n><C-w><C-v>:terminal<CR><Insert>")
